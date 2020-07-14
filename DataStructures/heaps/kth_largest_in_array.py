1d1d01a0fa9c57bcca6438609f3e34d8333b8bf8
Aditya <dramacha@akamai.com>
2020-07-14 08:03:52 -0700

    kth largest element

--- DataStructures/heaps/kth_largest_in_array.py
+++ DataStructures/heaps/kth_largest_in_array.py
@@ -3,51 +3,53 @@
 # SC: O(K)
 from heapq import *
 
-
 class Solution:
-    def brute_force_helper(self, nums, k):
+    def brute_force_helper(self, nums,k):
         nums.sort()
-        i = len(nums) - 1
-        while k > 0:
-            i -= 1
-            k -= 1
-        return nums[i + 1]
-
-    def heap_helper_k_largest(self, nums, k):
+        i=len(nums)-1
+        while k>0:
+            i-=1
+            k-=1
+        return nums[i+1]
+    def heap_helper_k_largest(self, nums,k):
         """
         TC: O(KlogK) + O((N-K)logK) ~ O(NlogK)
         SC: O(K)
         """
-        min_heap = list()
-
+        min_heap=list()
         for i in range(k):
-            heappush(min_heap, nums[i])
+            heappush(min_heap,nums[i])
 
         for i in range(k, len(nums)):
-            if nums[i] > min_heap[0]:
+            if nums[i]>min_heap[0]:
                 heappop(min_heap)
                 heappush(min_heap, nums[i])
-        print('kth largest ' + str(min_heap[0]))
+        
         return min_heap[0]
-
+    def heap_helper_2(self, nums,k):
+        for i in range(len(nums)):
+            heappush(min_heap,nums[i])
+            if len(min_heap)>k:
+                heappop(min_heap)
+        return min_heap[0]
     def heap_helper_k_smallest(self, nums, k):
         """
         TC: O(KlogK) + O((N-K)logK) ~ O(NlogK)
         """
-        max_heap = list()
-
+        max_heap=list()
+        
         for i in range(k):
             heappush(max_heap, -nums[i])
-
+            
         for i in range(k, len(nums)):
-            if -nums[i] > max_heap[0]:
+            if -nums[i]>max_heap[0]:
                 heappop(max_heap)
                 heappush(max_heap, -nums[i])
-        print('kth smallest ' + str(-max_heap[0]))
-
+        print('kth smallest '+ str(-max_heap[0]))
     def findKthLargest(self, nums: List[int], k: int) -> int:
         if not nums:
             return nums
         #return self.brute_force_helper(nums,k)
         self.heap_helper_k_smallest(nums, k)
-        return self.heap_helper_k_largest(nums, k)
+        return self.heap_helper_k_largest(nums,k)
+    
