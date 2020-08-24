ac3bcc15581ff982d094dddfe2d98576b4ff9acd
Adi <1384924+adityardesai@users.noreply.github.com>
2020-08-23 20:52:14 -0700

    Create design_hit_counter.py

---
+++ DataStructures/designs/design_hit_counter.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/design-hit-counter
+
+# Basic version, not scalable
+from collections import deque
+class HitCounter:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q=deque()
+    
+
+    def hit(self, timestamp: int) -> None:
+        """
+        Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity).
+        """
+        self.q.append(timestamp)
+        
+
+    def getHits(self, timestamp: int) -> int:
+        """
+        Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity).
+        """
+        while self.q and timestamp - self.q[0]>=300:
+            self.q.popleft()
+        return len(self.q)
+        
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
