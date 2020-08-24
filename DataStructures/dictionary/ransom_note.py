6b438ab86748bea524c11d9b04a2a52397f55f45
Adi <1384924+adityardesai@users.noreply.github.com>
2020-08-23 19:47:48 -0700

    Update ransom_note.py

--- DataStructures/dictionary/ransom_note.py
+++ DataStructures/dictionary/ransom_note.py
@@ -2,21 +2,23 @@
 # TC:O(m)
 # SC:O(1) alphabets are always 26 
 
-def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
     
-    # Check for obvious fail case.
-    if len(ransomNote) > len(magazine): return False
-
-    # In Python, we can use the Counter class. It does all the work that the
-    # makeCountsMap(...) function in our pseudocode did!
-    letters = collections.Counter(magazine)
-    
-    # For each character, c, in the ransom note:
-    for c in ransomNote:
-        # If there are none of c left, return False.
-        if letters[c] <= 0:
+        if len(ransomNote) > len(magazine): 
             return False
-        # Remove one of c from the Counter.
-        letters[c] -= 1
-    # If we got this far, we can successfully build the note.
-    return True
+
+        mag_hash_map = dict()
+
+        for ch in magazine:
+            mag_hash_map[ch] = mag_hash_map.get(ch, 0)+1
+
+        for c in ransomNote:
+            if c not in mag_hash_map:
+                return False
+            if mag_hash_map[c]<=0:
+                return False
+            mag_hash_map[c]-=1
+
+        return True
+        
