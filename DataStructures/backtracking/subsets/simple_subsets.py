24e4438a7706215f2a289fcb8722faad7cbd56e8
Aditya <dramacha@akamai.com>
2020-09-03 07:48:23 -0700

    adding simple subset

---
+++ DataStructures/backtracking/subsets/simple_subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backtrack(self, result, nums, temp, start_index):
+        result.append(list(temp))
+        for i in range(start_index, len(nums)):
+            temp.append(nums[i])
+            self.backtrack(result, nums, temp,i+1)
+            
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        
+        result = list()
+        start_index = 0
+        self.backtrack(result,nums, list(), start_index)
+        return result
+        
