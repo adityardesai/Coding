640c984a6af3ae0274a4ecc7e8c43d927a59ef5b
Adi <1384924+adityardesai@users.noreply.github.com>
2020-09-05 23:01:43 -0700

    Update insert_into_bst_recursion.py

--- DataStructures/MSFT/insert_into_bst_recursion.py
+++ DataStructures/MSFT/insert_into_bst_recursion.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/insert-into-a-binary-search-tree/
+# TC:O(H)
+# SC:O(H)
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
