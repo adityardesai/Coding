7d4015c24030046ad6ecdfaf22261a3ee8a2d8bd
Adi <1384924+adityardesai@users.noreply.github.com>
2020-09-15 23:13:12 -0700

    Update nX3Matrix_3_colors.py

--- DataStructures/dynamic/paint_house/nX3Matrix_3_colors.py
+++ DataStructures/dynamic/paint_house/nX3Matrix_3_colors.py
@@ -1,5 +1,14 @@
+"""
+You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colours: Red, Yellow or Green while making sure that no two adjacent cells have the same colour (i.e no two cells that share vertical or horizontal sides have the same colour).
 
-# 
+You are given n the number of rows of the grid.
+
+Return the number of ways you can paint this grid. As the answer may grow large, the answer must be computed modulo 10^9 + 7.
+
+ 
+
+"""
+# https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid/
 # TC: 
 # SC: 
 class Solution:
