8b70f4226c0cbaa09592843789ec3b115566d5c7
Adi <1384924+adityardesai@users.noreply.github.com>
2020-09-29 23:31:27 -0700

    Update read_n_chars.py

--- DataStructures/adhoc/read4/read_n_chars.py
+++ DataStructures/adhoc/read4/read_n_chars.py
@@ -58,3 +58,53 @@ class Solution:
         
         return copied_chars
         
+==================================
+#158 works here too
+
+
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def __init__(self):
+        self.tmp_count=0
+        self.tmp_ptr=0
+        self.tmp_buf=[''] * 4
+        
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        total=0
+        
+        while total<n:
+            
+            if self.tmp_ptr==0:
+                self.tmp_count=read4(self.tmp_buf)
+            
+            while total < n and self.tmp_ptr<self.tmp_count:
+                buf[total]=self.tmp_buf[self.tmp_ptr]
+                self.tmp_ptr+=1
+                total+=1
+            
+            if self.tmp_count<4:
+                break
+            
+            if self.tmp_ptr == self.tmp_count: 
+                self.tmp_ptr=0
+        
+        return total
