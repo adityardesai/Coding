58f325853343edb06ca97192638dd5b480873a8c
Adi <1384924+adityardesai@users.noreply.github.com>
2020-09-29 18:02:45 -0700

    Update unique_paths_robot.py

--- DataStructures/dynamic/unique_paths_robot.py
+++ DataStructures/dynamic/unique_paths_robot.py
@@ -9,7 +9,7 @@ class Solution:
             m, n - 1)
 
     def dynamic_helper(self, m, n):
-        dp = [[1] * n for x in range(m)]
+        dp=[[1 for x in range(n)] for x in range(m)]
         for i in range(1, m):
             for j in range(1, n):
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
