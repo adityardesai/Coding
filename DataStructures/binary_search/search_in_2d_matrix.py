d5587ba771fb49bc4d7710f042927005fe31b436
Adi <1384924+adityardesai@users.noreply.github.com>
2020-09-30 21:42:23 -0700

    Create search_in_2d_matrix.py

---
+++ DataStructures/binary_search/search_in_2d_matrix.py
@@ -0,0 +1,43 @@
+"""
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+
+TC:O(logN)
+SC:O(1)
+"""
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        M = len(matrix)
+        if M == 0:
+            return False
+        N = len(matrix[0])
+        
+        left = 0
+        right = M*N - 1
+        
+        while left <= right:
+            mid_idx = (left + right) //2
+            mid_ele = matrix[mid_idx // N][mid_idx % N]
+            
+            if target == mid_ele:
+                return True
+            elif target < mid_ele:
+                right = mid_idx-1
+            else:
+                left = mid_idx+1
+                    
+        return False
